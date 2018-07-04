afb57dc57e4d94d0034518f415236057064d63c3
iryudragon01 <iryudragon01@gmail.com>
2018-07-04 18:05:17 +0700

    add adapter to showview

--- app/src/main/java/com/iryu/wahkor/testgitjuly04/MainActivity.kt
+++ app/src/main/java/com/iryu/wahkor/testgitjuly04/MainActivity.kt
@@ -2,13 +2,15 @@ package com.iryu.wahkor.testgitjuly04
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 class item(val Name:String,var first:Int,var last:Int ,val price:Int,var Active:Boolean)
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var mylist:MutableList<String>
         var fooditem:MutableList<item>
         fooditem=ArrayList<item>()
         fooditem.add(0,item("mama",30,20,15,true))
@@ -17,12 +19,9 @@ class MainActivity : AppCompatActivity() {
         fooditem.add(fooditem.size,item("oishi" ,16,15,12,true))
         fooditem.add(fooditem.size,item("water" ,30,17,5 ,true))
         fooditem.add(fooditem.size,item("est"   ,72,30,10,true))
-        fooditem.forEach { subfood ->
-            println("${ subfood.Name}= ${subfood.first}" )
-            subfood.first++
-        }
-        fooditem.forEach { subfood ->
-            println("${ subfood.Name}= ${subfood.first}" )
-        }
+    println("fooditem size =${fooditem.size}")
+       var myadape=foodadape(this,fooditem)
+        showview.layoutManager=LinearLayoutManager(this)
+        showview.adapter=myadape
     }
 }
---
+++ app/src/main/java/com/iryu/wahkor/testgitjuly04/adape.kt
@@ -0,0 +1,31 @@
+package com.iryu.wahkor.testgitjuly04
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.adape.view.*
+
+class foodadape(var context: Context,private var itemlist:MutableList<item> ):RecyclerView.Adapter<viewholder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewholder {
+        val view=LayoutInflater.from(parent?.context).inflate(R.layout.adape,parent,false)
+
+        return viewholder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemlist.size
+    }
+
+    override fun onBindViewHolder(holder: viewholder, position: Int) {
+        holder?.view?.name.text=itemlist[position].Name
+        holder?.view?.first.text=itemlist[position].first.toString()
+        holder?.view?.last.text=itemlist[position].last.toString()
+          }
+
+
+}
+class viewholder(val view:View):RecyclerView.ViewHolder(view){
+
+}
