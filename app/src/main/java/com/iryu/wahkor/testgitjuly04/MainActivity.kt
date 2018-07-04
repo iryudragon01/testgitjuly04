b9745f1665bbc4af3a15cb0a92b8bce98b596d68
iryudragon01 <iryudragon01@gmail.com>
2018-07-04 13:38:45 +0700

    2nd mutable data class

--- app/src/main/java/com/iryu/wahkor/testgitjuly04/MainActivity.kt
+++ app/src/main/java/com/iryu/wahkor/testgitjuly04/MainActivity.kt
@@ -2,7 +2,7 @@ package com.iryu.wahkor.testgitjuly04
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-class item(val Name:String,var Vulume:Int,val Active:Boolean)
+class item(val Name:String,var first:Int,var last:Int ,val price:Int,var Active:Boolean)
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,18 +11,18 @@ class MainActivity : AppCompatActivity() {
         var mylist:MutableList<String>
         var fooditem:MutableList<item>
         fooditem=ArrayList<item>()
-        fooditem.add(0,item("mama",7,true))
-        fooditem.add(1,item("snack",24,true))
-        fooditem.add(2,item("jubjai",30,true))
-        fooditem.add(3,item("oishi",15,true))
-        fooditem.add(4,item("water",40,true))
-        fooditem.add(fooditem.size,item("est",66,true))
+        fooditem.add(0,item("mama",30,20,15,true))
+        fooditem.add(fooditem.size,item("snack" ,24,13,5 ,true))
+        fooditem.add(fooditem.size,item("jubjai",24,16,10,true))
+        fooditem.add(fooditem.size,item("oishi" ,16,15,12,true))
+        fooditem.add(fooditem.size,item("water" ,30,17,5 ,true))
+        fooditem.add(fooditem.size,item("est"   ,72,30,10,true))
         fooditem.forEach { subfood ->
-            println("${ subfood.Name}= ${subfood.Vulume}" )
-            subfood.Vulume++
+            println("${ subfood.Name}= ${subfood.first}" )
+            subfood.first++
         }
         fooditem.forEach { subfood ->
-            println("${ subfood.Name}= ${subfood.Vulume}" )
+            println("${ subfood.Name}= ${subfood.first}" )
         }
     }
 }
