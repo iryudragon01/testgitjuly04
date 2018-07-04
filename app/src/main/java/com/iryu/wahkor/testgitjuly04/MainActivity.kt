83724bf8cf9e20394aac81b3698e219a7835a668
iryudragon01 <iryudragon01@gmail.com>
2018-07-04 13:27:30 +0700

    mutable data class

--- app/src/main/java/com/iryu/wahkor/testgitjuly04/MainActivity.kt
+++ app/src/main/java/com/iryu/wahkor/testgitjuly04/MainActivity.kt
@@ -2,11 +2,27 @@ package com.iryu.wahkor.testgitjuly04
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+class item(val Name:String,var Vulume:Int,val Active:Boolean)
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var mylist:MutableList<String>
+        var fooditem:MutableList<item>
+        fooditem=ArrayList<item>()
+        fooditem.add(0,item("mama",7,true))
+        fooditem.add(1,item("snack",24,true))
+        fooditem.add(2,item("jubjai",30,true))
+        fooditem.add(3,item("oishi",15,true))
+        fooditem.add(4,item("water",40,true))
+        fooditem.add(fooditem.size,item("est",66,true))
+        fooditem.forEach { subfood ->
+            println("${ subfood.Name}= ${subfood.Vulume}" )
+            subfood.Vulume++
+        }
+        fooditem.forEach { subfood ->
+            println("${ subfood.Name}= ${subfood.Vulume}" )
+        }
     }
 }
